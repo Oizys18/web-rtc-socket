85ddd41614f7aac23b1bf5823b8615fd225f2806
Yang Chan Woo <oizys18@gmail.com>
2021-12-29 23:48:30 +0900

    Add application get home

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 
 const app = express();
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
 
-const handleListen = () => console.log("Hello Local 3000");
+app.get("/", (req, res) => res.render("home"));
+const handleListen = () => console.log("Hello http://localhost:3000");
 
 app.listen(3000, handleListen);
